bcde566da9311ae482dd4e145bf026a91ea99916
aderio25 <adewipo@gmail.com>
2021-08-11 07:55:45 +0700

    initial commit

---
+++ src/screens/Beranda.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from '../styles/android/Beranda.style';
+
+export default function Beranda() {
+    return (
+        <View style={styles.container}>
+            <Text>Hello World</Text>
+        </View>
+    )
+}
